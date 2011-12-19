764f251e3947108ed6dd622377ff807bdf93f802
Paul Bailey <paul.m.bailey@gmail.com>
2011-12-19 16:44:25 -0600

    initial revision

---
+++ untracked.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+import glob
+import shutil
+import argparse
+import ConfigParser
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Update Gondor config with multiple untracked files')
+  parser.add_argument('directories', metavar='DIR', nargs='+', help='Directory to recurse')
+  parser.add_argument('--config', dest='config', default='.gondor/config', help='Path to config file, Default: .gondor/config')
+  parser.add_argument('--configold', dest='configold', default='.gondor/config.old', help='Path to copy old config file, Default: .gondor/config.old')
+  parser.add_argument('--exclude', dest='exclude', action='append', default=['*.pyc', '.*'], help='Exclude files. Default: *.pyc., .*')
+  parser.add_argument('--excludedir', dest='excludedir', action='append', default=['.git',], help='Exclude directories. Default: .git')
+  
+  args = parser.parse_args()
+  
+  shutil.copyfile(args.config, args.configold)
+  config = ConfigParser.RawConfigParser()
+  config.read(args.config)
+  
+  include_files = []
+  
+  prefix = os.path.basename(os.getcwd())
+  
+  for d in args.directories:
+    for root, dirs, files in os.walk(d):
+      skip = False
+      for ed in args.excludedir:
+        if re.search(re.escape(ed), root):
+          skip = True
+          break
+          
+      if skip:
+        continue
+        
+      else:
+        tfiles = [os.path.join(root, f) for f in files]
+        bad = []
+        for ef in args.exclude:
+          globs = glob.glob(os.path.join(root, ef))
+          for f in tfiles:
+            if f in globs:
+              bad.append(f)
+              
+        for b in bad:
+          tfiles.remove(b)
+          
+        include_files.extend(tfiles)
+        
+  include_str = ''
+  for f in include_files:
+    include_str += os.path.join(prefix, f) + '\n'
+    
+  include_str = include_str[:-1]
+  if not config.has_section('files'):
+    config.add_section('files')
+    
+  config.set('files', 'include', include_str)
+  with open(args.config, 'wb') as configfile:
+    config.write(configfile)
+    
