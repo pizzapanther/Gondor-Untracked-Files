59cbd35ade57036b5b10fc8b00a50a23b72b5c72
Paul Bailey <paul.m.bailey@gmail.com>
2011-12-20 09:51:43 -0600

    fixed exclude and updated README

--- untracked/__main__.py
+++ untracked/__main__.py
@@ -13,8 +13,8 @@ def main():
   parser.add_argument('directories', metavar='DIR', nargs='+', help='Directory to recurse')
   parser.add_argument('--config', dest='config', default='.gondor/config', help='Path to config file, Default: .gondor/config')
   parser.add_argument('--configold', dest='configold', default='.gondor/config.old', help='Path to copy old config file, Default: .gondor/config.old')
-  parser.add_argument('--exclude', dest='exclude', action='append', default=['*.pyc', '.*'], help='Exclude files. Default: *.pyc., .*')
-  parser.add_argument('--excludedir', dest='excludedir', action='append', default=['.git',], help='Exclude directories. Default: .git')
+  parser.add_argument('--exclude', dest='exclude', action='append', default=[], help='Exclude files. Default: *.pyc., .*')
+  parser.add_argument('--excludedir', dest='excludedir', action='append', default=[], help='Exclude directories. Default: .git')
   
   args = parser.parse_args()
   
@@ -26,10 +26,18 @@ def main():
   
   prefix = os.path.basename(os.getcwd())
   
+  exclude = ['*.pyc', '.*']
+  if args.exclude:
+    exclude = args.exclude
+    
+  excludedir = ['.git']
+  if args.excludedir:
+    excludedir = args.excludedir
+    
   for d in args.directories:
     for root, dirs, files in os.walk(d):
       skip = False
-      for ed in args.excludedir:
+      for ed in excludedir:
         if re.search(re.escape(ed), root):
           skip = True
           break
@@ -40,7 +48,7 @@ def main():
       else:
         tfiles = [os.path.join(root, f) for f in files]
         bad = []
-        for ef in args.exclude:
+        for ef in exclude:
           globs = glob.glob(os.path.join(root, ef))
           for f in tfiles:
             if f in globs:
